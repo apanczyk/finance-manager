db3d6b527ab611c34e58ecbab2ece737116e939c
apanczyk <56263549+apanczyk@users.noreply.github.com>
2021-10-30 18:13:13 +0200

    [F] Add button implementation (#9)

---
+++ ui/src/components/main/OperationForm.tsx
@@ -0,0 +1,125 @@
+import { Dialog, DialogContent, DialogTitle, Grid, Input, RadioGroup, Select, Typography } from "@mui/material";
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import CloseIcon from '@mui/icons-material/Close';
+import React, { Dispatch, SetStateAction } from "react";
+import Operation from "../../model/Operation";
+import Button from "@material-ui/core/Button";
+import * as Yup from "yup";
+import { Field, Form, Formik } from "formik";
+import { TextField } from 'formik-mui';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        dialogWrapper: {
+            padding: theme.spacing(2),
+            position: 'absolute',
+            top: theme.spacing(5)
+        },
+        dialogTitle: {
+            paddingRight: '0px'
+        },
+    }),
+);
+
+const initialFValues: Operation = {
+    id: "",
+    name: "",
+    amount: 0,
+    place: "",
+    date: ""
+}
+
+interface OperationFormProps {
+    openPopup: boolean
+    setOpenPopup: Dispatch<SetStateAction<boolean>>
+    recordForEdit: any
+    addOrEdit: (operation: Operation, resetForm: any) => void
+}
+
+export default function OperationForm(props: OperationFormProps) {
+    const [values, setValues] = React.useState(initialFValues);
+    const classes = useStyles();
+    const { openPopup, setOpenPopup, recordForEdit, addOrEdit } = props;
+
+    const resetForm = () => {
+        setValues(initialFValues);
+    }
+
+    const handleSubmit = (formValue: { name: string; amount: number, place: string, date: string }) => {
+        const { name, amount, place, date } = formValue;
+        const data: Operation = {
+            id: "",
+            name: name,
+            amount: amount,
+            place: place,
+            date: date
+        };
+
+        addOrEdit(data, resetForm);        
+    }
+
+    const validationSchema = () => {
+        return Yup.object().shape({
+            name: Yup.string().required("This field is required!"),
+            amount: Yup.string().required("This field is required!"),
+            place: Yup.string().required("This field is required!"),
+            date: Yup.string().required("This field is required!")
+        });
+    }
+
+    React.useEffect(() => {
+        console.log("")
+        if (recordForEdit != null)
+            setValues({
+                ...recordForEdit
+            })
+    }, [recordForEdit])
+
+    return (
+        <Dialog
+            open={openPopup}
+            maxWidth="md" classes={{ paper: classes.dialogWrapper }}>
+            <DialogTitle className={classes.dialogTitle}>
+                <div style={{ display: 'flex' }}>
+                    <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
+                        Operation form
+                    </Typography>
+                    <Button
+                        color="secondary"
+                        onClick={() => { setOpenPopup(false) }}
+                    >
+                        <CloseIcon />
+                    </Button>
+                </div>
+            </DialogTitle>
+            <DialogContent dividers>
+                <Formik
+                    initialValues={initialFValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    <Form>
+                        <Grid container>
+                            <Grid item xs={6}>
+                                <Field component={TextField} name="name" type="text" label="name" disabled="" variant="standard" fullWidth />
+                                <Field component={TextField} name="amount" type="number" label="amount" disabled="" variant="standard" fullWidth />
+                                <Field component={TextField} name="place" type="text" label="place" disabled="" variant="standard" fullWidth />
+                                <Field component={TextField} name="date" type="text" label="date" disabled="" variant="standard" fullWidth />
+                            </Grid>
+                        </Grid>
+
+                        <div>
+                            <Button type="submit">
+                                Submit
+                            </Button>
+                            <Button onClick={resetForm}>
+                                Reset
+                            </Button>
+                        </div>
+
+                    </Form>
+                </Formik>
+            </DialogContent>
+        </Dialog>
+    )
+}
