c99aab14f588db2d76c379ee6d1adcf0d3f609c6
Arczg <apanczyk98@gmail.com>
2021-12-31 20:41:39 +0100

    [B] Diagram first improve

--- server/src/main/kotlin/pl/ap/finance/service/WalletService.kt
+++ server/src/main/kotlin/pl/ap/finance/service/WalletService.kt
@@ -6,17 +6,20 @@ import pl.ap.finance.exceptions.WalletNotFoundException
 import pl.ap.finance.model.Operation
 import pl.ap.finance.model.Wallet
 import pl.ap.finance.model.dto.OperationDto
+import pl.ap.finance.model.response.GroupedOperation
 import pl.ap.finance.repository.CategoryRepository
 import pl.ap.finance.repository.WalletRepository
+import java.time.LocalDate
+import java.util.*
 
 @Service
 class WalletService(private val walletRepository: WalletRepository, private val categoryRepository: CategoryRepository) {
 
     fun addOperation(walletId: Long, operationDto: OperationDto): Wallet {
+        val category = categoryRepository.findById(operationDto.category)
         val wallet = walletRepository.findById(walletId).orElseThrow {
             throw WalletNotFoundException("Wallet with id $walletId doesn't exist")
         }
-        val category = categoryRepository.findById(operationDto.category)
         val operation = Operation(
             name = operationDto.name,
             amount = operationDto.amount,
@@ -31,5 +34,13 @@ class WalletService(private val walletRepository: WalletRepository, private val 
     fun getWallets(@PathVariable("id") userId: Long): List<Wallet> {
         val wallets = walletRepository.findAll()
         return wallets
+    }
+
+    fun groupOperations(wallet: Wallet): List<GroupedOperation> {
+        val operations = wallet.operations
+
+        val groupedOperation: List<GroupedOperation> = emptyList()
+        operations.sortedBy { it.date }.filter { it.date > LocalDate.now().minusYears(1) }
+        return groupedOperation
     }
 }
