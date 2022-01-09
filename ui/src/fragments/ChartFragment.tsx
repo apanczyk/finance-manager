c284f8dd522687bf961b03e7d06beef860e69b66
Arczg <apanczyk98@gmail.com>
2022-01-09 21:44:45 +0100

    [F] Partial front chart

--- ui/src/fragments/ChartFragment.tsx
+++ ui/src/fragments/ChartFragment.tsx
@@ -1,18 +1,66 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Dialog, DialogContent, DialogTitle, Typography } from '@mui/material';
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, PieChart, Pie, Sector, Legend, Cell } from 'recharts';
 import DataService from '../service/api/DataService';
 import GroupedOperation from '../model/GroupedOperation';
 import Operation from '../model/Operation';
+import CloseIcon from '@mui/icons-material/Close';
 
 interface ChartProps {
     wallet: string,
     operations: Array<Operation>
 }
 
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF'];
+
+const pieData = [
+    {
+        "name": "Chrome",
+        "value": 68.85
+    },
+    {
+        "name": "Firefox",
+        "value": 7.91
+    },
+    {
+        "name": "Edge",
+        "value": 6.85
+    },
+    {
+        "name": "Internet Explorer",
+        "value": 6.14
+    },
+    {
+        "name": "Others",
+        "value": 10.25
+    }
+];
+
+const CustomTooltip = ({ active, payload, label }: any) => {
+    if (active) {
+        return (
+            <div className="custom-tooltip" style={{ backgroundColor: '#ffff', padding: '5px', border: '1px solid #cccc' }}>
+                <label>{`${payload[0].name} : ${payload[0].value}%`}</label>
+            </div>
+        );
+    }
+
+    return null;
+};
+
 export default function ChartFragment(props: ChartProps) {
     const [groupedOperations, setGroupedOperations] = React.useState<Array<GroupedOperation>>();
     const { wallet, operations } = props
+    const [modal, setModal] = React.useState<boolean>(false)
+    const [month, setMonth] = React.useState<string>()
+
+    const monthClick = (event: any) => {
+        if (event.activeLabel) {
+            setMonth(event.activeLabel)
+            setModal(true)
+        }
+        else setModal(false)
+    };
 
     React.useEffect(() => {
         DataService.getGroupedOperations(wallet)
@@ -32,6 +80,7 @@ export default function ChartFragment(props: ChartProps) {
                 </Typography >
                 <ResponsiveContainer aspect={3}>
                     <LineChart
+                        onClick={monthClick}
                         width={150}
                         height={150}
                         data={groupedOperations}
@@ -50,6 +99,44 @@ export default function ChartFragment(props: ChartProps) {
                     </LineChart>
                 </ResponsiveContainer>
             </Box>
+
+
+
+            <Dialog
+                open={modal}
+                maxWidth="md"
+                fullWidth >
+                <DialogTitle>
+                    <div style={{ display: 'flex' }}>
+                        <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
+                            Chart for {month}
+                        </Typography>
+                        <Button
+                            color="secondary"
+                            onClick={() => {
+                                setMonth(undefined)
+                                setModal(false)
+                            }}
+                        ><CloseIcon />
+                        </Button>
+                    </div>
+                </DialogTitle>
+                <DialogContent>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+                        <ResponsiveContainer width="100%" height={250}>
+                            <PieChart width={730} height={300}>
+                                <Pie data={pieData} color="#000000" dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={120} fill="#8884d8" >
+                                    {
+                                        pieData.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                                    }
+                                </Pie>
+                                <Tooltip content={<CustomTooltip />} />
+                                <Legend />
+                            </PieChart>
+                        </ResponsiveContainer>
+                    </Box>
+                </DialogContent >
+            </Dialog >
         </>
     );
 }
